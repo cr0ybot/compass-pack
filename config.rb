d55343556f9d1592312de98000c48420bf254111
Cory Hughart <cr0ybot@gmail.com>
2015-03-26 19:25:43 -0400

    added autoprefixer, minor variable adjustments

--- config.rb
+++ config.rb
@@ -1,3 +1,24 @@
+# This is "Compass" pack, after all
+require "compass"
+
+# I don't like to bother with compass vendor prefix helpers, so I use autoprefixer
+# https://github.com/postcss/autoprefixer#compass
+require "autoprefixer-rails"
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
+
 preferred_syntax = :scss
 
 http_path = '/'
